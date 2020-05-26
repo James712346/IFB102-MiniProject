7973c8fe20af9fe47f5204e1979af2b2b2ecbdd1
james <james712346@gmail.com>
2020-05-26 12:09:32 +1000

    Update

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -2,7 +2,7 @@ var synth = new oscillators.push(new Tone.Oscillator({
 				"frequency" : 420,
 				"type" : "sawtooth10",
 				"volume" : -Infinity,
-				"detune" : Math.random() * 30 - 15}).toMaster());.toMaster()
+				"detune" : Math.random() * 30 - 15}).toMaster());
 var ws = new WebSocket('ws://' + document.domain + ':' + location.port + '/ws');
 
 var sides = [0,1];
