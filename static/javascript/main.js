87300de2f68f9a7c7661ff094f9d03861eec5949
james <james712346@gmail.com>
2020-05-26 12:39:53 +1000

    Trying to get rampto to work

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,8 +1,5 @@
-var synth = new Tone.Oscillator({
-				"frequency" : 420,
-				"type" : "sawtooth10",
-				"volume" : -Infinity,
-				"detune" : Math.random() * 30 - 15}).toMaster();
+var oscillator = ""
+
 var ws = new WebSocket('ws://' + document.domain + ':' + location.port + '/ws');
 
 var sides = [0,1];
@@ -23,29 +20,31 @@ $("#swap").click(function() {
 });
 
 $("#play").click(function() {
+	var context = new AudioContext();
   $('#play').hide()
   $('#stop').show()
-  var context = new AudioContext();
-  synth.start();
+	var oscillator = new Tone.Oscillator({"frequency" : 440,"type" : "sawtooth10","volume" : -Infinity,"detune" : Math.random() * 30 - 15}).toMaster();
+  oscillator.start();
   ws.onmessage = function (event) {
        data = JSON.parse(event.data);
        //percentage of Distance to Max Distance times range of the Frequancy
        freq = ((parseFloat(data[1])-Dranges[sides[1]][0]/Dranges[sides[1]][1]) * (Vranges[1][1] - Vranges[1][0])) + Vranges[1][0];
        volume = ((parseFloat(data[0])-Dranges[sides[0]][0]/Dranges[sides[0]][1])*(Vranges[0][1] - Vranges[0][0])) + Vranges[0][0];//give a percentage of how intense the note will be
        freq = parseFloat(freq).toFixed(2)
-       volume = parseFloat(volume/100).toFixed(2)
+       volume = parseInt(volume)
        console.log("Freq = "+ freq);
        console.log("Volume = "+ volume);
        if (freq < Vranges[1][1] && freq > Vranges[1][0]){
-         synth.frequency.rampTo(freq, 1)
-         synth.volume.rampTo(volume, 1)
+         oscillator.frequency.rampTo(freq, 1)
+         oscillator.volume.rampTo(volume, 1)
        }
 		 }
 });
 $('#stop').click(function() {
   $('#stop').hide()
   $('#play').show()
-  synth.stop();
+  oscillator.stop();
+	var oscillator = ""
   ws.onmessage = ""
 });
 
