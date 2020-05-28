219945912ef7b870088d51e8e0da2d91c51d8cdf
james <james712346@gmail.com>
2020-05-28 12:21:32 +1000

    Having a go at being able to have multiple clinets connected at once

--- README.md
+++ README.md
@@ -10,11 +10,11 @@
 
 
 
-Statement of contribution (see below) 
+Statement of contribution (see below)
 
 
 
-Project Objectives (0.5 page max): 
+Project Objectives (0.5 page max):
 
 [^]: What is the goal of the project?
 
@@ -22,18 +22,19 @@ This Project will use the concept of a Theremin, this device used
 
 
 
- I will achieve this by using distance sensors, two of them, one for the pitch and the other controls the volume of the note. 
+ I will achieve this by using distance sensors, two of them, one for the pitch and the other controls the volume of the note.
 
-Review and Discussion of Technologies Used (5 pages max): 
+Review and Discussion of Technologies Used (5 pages max):
 
 [^]: High level discussion of the technologies used, why were they used, alternatives, how they work (high level) and how they relate to the technologies presented in lectures.
 
 
 
-Design and Implementation (5 pages max): 
+Design and Implementation (5 pages max):
 
 [^]: Discuss details of the design and implementation, how it works, challenges you faced, configuration, experimentation, results, etc. Include a system diagram and discuss future directions / improvements.
 
+https://github.com/James712346/IFB102-MiniProject
 
 
 References (1 page max):
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from quart import websocket, Quart, render_template
 from Bluetin_Echo import Echo
 import time
+import asyncio
 
 app = Quart(__name__)
 Pins = ((22,18),(23,17))
@@ -8,16 +9,31 @@ speed = 320
 samples = 5
 Distance = [Echo(*pin ,speed) for pin in Pins]
 print(Distance[0].read('cm'))
+clients = set()
+
+async def SendtoWebsocket():
+    while True:
+        reads = [ round(d.read('cm',samples),1) for d in Distance ]
+        for websocket in clients:
+            try:
+                await websocket.send(str(reads))
+            except asyncio.CancelledError:
+                print("Client Disconnected")
+        await asyncio.sleep(0.05) #Give 0.05 seconds to the webserver for other tasks
+
 
 @app.websocket('/ws')
 async def ws():
-    while True:
-        reads = [ round(d.read('cm',samples),1) for d in Distance ]
-        print(reads)
-        await websocket.send(str(reads))
+    Webserver_Loop = asyncio.get_event_loop()
+    clients.add(websocket._get_current_object())
+    if (asyncio.all_tasks()):
+        asyncio.create_task(SendtoWebsocket(), name="Web")
+    print(asyncio.all_tasks())
+
 
 @app.route('/', methods=['GET'])
 async def posts():
     return await render_template('index.html')
+
 
 app.run(host='0.0.0.0',port=81)
